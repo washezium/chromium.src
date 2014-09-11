f7b7a8bb99a0cc72025e85ddf0f1270e87fc91f1
Jan Engelhardt <jengelh@inai.de>
2014-09-11 16:35:12 +0200

    prefs: disable "Use a web service to help resolve navigation errors"
    This goes towards reducing promiscuous outbound requests to
    some central services.
    
    Impl. details:
    Set alternate_error_pages to be disabled by default.

--- chrome/browser/ui/navigation_correction_tab_observer.cc
+++ chrome/browser/ui/navigation_correction_tab_observer.cc
@@ -57,7 +57,7 @@ NavigationCorrectionTabObserver::~NavigationCorrectionTabObserver() {
 void NavigationCorrectionTabObserver::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* prefs) {
   prefs->RegisterBooleanPref(prefs::kAlternateErrorPagesEnabled,
-                             true,
+                             false,
                              user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 }
 
