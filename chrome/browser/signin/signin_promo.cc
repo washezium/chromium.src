082ed13d67dce7371e6583d7f77544ce61395a2f
Jan Engelhardt <jengelh@inai.de>
2014-11-09 01:54:27 +0100

    promo: disable Google promotion fetching
    Documentation is scarce, and sprinkled with misleading acronyms.
    NTP is not NTP, for example. FWIW:
    
    // A PromoResourceService fetches data from a web resource server to
    // be used to dynamically change the appearance of the New Tab Page.
    // For example, it has been used to fetch "tips" to be displayed on
    // the NTP, or to display promotional messages to certain groups of
    // Chrome users.
    
    Whatever it is that it downloads, deactivate the one that leads to
    Google.
    
    References: https://github.com/iridium-browser/iridium-browser/issues/33

--- chrome/browser/signin/signin_promo.cc
+++ chrome/browser/signin/signin_promo.cc
@@ -171,7 +171,7 @@ bool IsAutoCloseEnabledInEmbeddedURL(const GURL& url) {
 
 void RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
-  registry->RegisterBooleanPref(prefs::kSignInPromoShowOnFirstRunAllowed, true);
+  registry->RegisterBooleanPref(prefs::kSignInPromoShowOnFirstRunAllowed, false);
   registry->RegisterBooleanPref(prefs::kSignInPromoShowNTPBubble, false);
   registry->RegisterIntegerPref(prefs::kDiceSigninUserMenuPromoCount, 0);
 }
